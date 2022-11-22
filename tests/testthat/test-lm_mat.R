7a80ecd1a64a88e5c18caf81b0c59d05a3e083a5
shushunr <shushunr@umich.edu>
2022-11-22 02:22:06 -0500

    modified test-lm_mat.R

--- tests/testthat/test-lm_mat.R
+++ tests/testthat/test-lm_mat.R
@@ -13,6 +13,6 @@ test_that("lm_mat cannot deal with variables' name not in the given dataset", {
 
 test_that("lm_mat cannot deal with variables' name not in the given dataset", {
   model <- lm_mat("mpg", c("drat", "disp", "wt"), mtcars, beta0 = TRUE)
-  X <- matrix(c(rep(1,nrow(data)), data[,x[1]], data[,x[2]]),nrow(data),3)
+  X <- matrix(c(rep(1,nrow(mtcars)), mtcars[,"drat"], mtcars[,"disp"], mtcars[,"wt"]),nrow(mtcars),4)
   expect_equal(model$H, X %*% solve(t(X) %*% X) %*% t(X))
 })
